89550324d4886f145e56ead3fd44bbeace4801f8
Sheby <83452219+Shebyyy@users.noreply.github.com>
2025-02-16 16:39:46 +0530

    Update hanime.js

--- javascript/anime/src/zh/hanime.js
+++ javascript/anime/src/zh/hanime.js
@@ -5,7 +5,7 @@ const mangayomiSources = [{
   "apiUrl": "",
   "iconUrl": "https://vdownload.hembed.com/image/icon/tab_logo.png?secure=EJYLwnrDlidVi_wFp3DaGw==,4867726124",
   "typeSource": "single",
-  "isManga": false,
+  "isManga": true,
   "isNsfw": true,
   "version": "0.0.1",
   "dateFormat": "",
