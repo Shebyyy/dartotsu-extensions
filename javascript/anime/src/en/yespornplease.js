41c4b759ce8275709bf41f4bb989b7b917949c8a
Sheby <83452219+Shebyyy@users.noreply.github.com>
2025-03-07 12:10:06 +0530

    Update yespornplease.js

--- javascript/anime/src/en/yespornplease.js
+++ javascript/anime/src/en/yespornplease.js
@@ -7,6 +7,7 @@ const mangayomiSources = [
 		"iconUrl": "https://yespornpleasexxx.com/wp-content/uploads/2023/12/XXX-Yespornplease-XXX-Favicon.png",
 		"typeSource": "single",
 		"itemType": 1,
+		"isNsfw": true,
 		"version": "0.0.2",
 		"pkgPath": "anime/src/en/yespornplease.js"
 	}
